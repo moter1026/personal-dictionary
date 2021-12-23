42ca03588fa82a3b82b5dc7dc4fe2cd8b9b407c0
moter1026 <matvej.sidoroff2016@yandex.ru>
2021-12-23 12:51:10 +0300

    23.12.21

--- js/account.js
+++ js/account.js
@@ -64,6 +64,49 @@ function getAllWords() {
                 }
                 $("#table_with_all_words > tbody").append(toHTML);
 
+                let rowEl = $("#table_with_all_words tbody tr");
+                let $pencilRedact = $(`<img src="./img/pencil.svg" class="pencil_redact" id="pencil_redact">`);
+                rowEl.on("mouseenter", (event) => {
+                    let tr = event.target.parentElement;
+                    if (tr.children[1].firstChild.nodeName == "#text") {
+                        tr.children[1].append($pencilRedact[0]);
+                    } 
+                });
+
+                $pencilRedact.on("click", (ev) => {
+                    let tr = ev.target.parentElement.
+                    parentElement;
+                    let word = tr.children[0].textContent;
+                    let values = tr.children[1].textContent;
+                    tr.children[0].textContent = '';
+                    tr.children[1].textContent = '';
+                        // Если есть уже инпуты для редактированияЮ то удалить его и 
+                        // вставить новые для нужного слова
+                    if ($("#redactWord")[0]) {
+                        let wordFromInput = $("#redactWord")[0].value;
+                        let valuesFromInput = $("#redactValue")[0].value;
+                        let parentTR = $("#redactWord")[0].parentElement.parentElement;
+                        $("#redactWord")[0].remove();
+                        $("#redactValue")[0].remove();
+                        parentTR.children[0].textContent = wordFromInput;
+                        parentTR.children[1].textContent = valuesFromInput;
+                    }
+                    tr.children[0].append(
+                        $(`<input name="redactWord" id="redactWord" value="${word}"></input>`)[0]);
+                    tr.children[1].append(
+                        $(`<input name="redactValue" id="redactValue" value="${values}"></input>`)[0]);
+                    // tr.children[1].lastChild.remove();
+                    // tr.children[1].append($(``))
+
+                })
+
+                rowEl.on("mouseleave", (event) => {
+                    let tr = event.target.parentElement;
+                    if (tr.children[1].firstChild.nodeName == "#text") {
+                        tr.children[1].lastChild.remove();    
+                    }
+                    // tr.children[1].children[0].style.display = "none";
+                });
             }
         })
     } catch (err) {
